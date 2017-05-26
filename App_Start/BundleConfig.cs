e9158a55fea0b934c3bf1b2b8e91be7d9ff557d1
creyes-dev <creyes.dev@gmail.com>
2017-05-26 02:28:02 -0300

    Bootstrap 3 layout test

---
+++ App_Start/BundleConfig.cs
@@ -0,0 +1,40 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace MvcApplication2
+{
+    public class BundleConfig
+    {
+        // Para obtener más información acerca de Bundling, consulte http://go.microsoft.com/fwlink/?LinkId=254725
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+                        "~/Scripts/jquery-ui-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.unobtrusive*",
+                        "~/Scripts/jquery.validate*"));
+
+            // Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información sobre los formularios. De este modo, estará
+            // preparado para la producción y podrá utilizar la herramienta de creación disponible en http://modernizr.com para seleccionar solo las pruebas que necesite.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap*"));
+
+            bundles.Add(new StyleBundle("~/Content/bootstrap/css").Include(
+                "~/Content/css/bootstrap.css",
+                "~/Content/css/bootstrap-theme.css"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
+                        "~/Content/themes/base/jquery-ui.css",
+                        "~/Content/themes/base/jquery-ui.structure.css",
+                        "~/Content/themes/base/jquery-ui.theme.css"));
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Test()
+        {
+            return View();
+        }
+
+        public ActionResult BootstrapStructureTest()
+        {
+            return View();
+        }
+
+        public ActionResult jqueryuitest()
+        {
+            return View();
+        }
+
+    }
+}
